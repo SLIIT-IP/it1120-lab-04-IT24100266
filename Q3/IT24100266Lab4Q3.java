5e6aa1ea7731fb615720cd641a7b507ffbcd5918
IT24100266 <it24100266@my.sliit.lk>
2024-08-19 16:35:57 +0530

    Add files via upload

---
+++ Q3/IT24100266Lab4Q3.java
@@ -0,0 +1,15 @@
+public class IT24100266Lab4Q3{
+     public static void main(String[]args){
+ 
+        int number = 37;
+           System.out.println("Enter a number:37");
+
+          if (number>0){
+             System.out.println("The number is: positive.");}
+          else if (number<0){
+              System.out.println("The number is: negative");}
+          else {
+             System.out.println("The number is: zero"); }
+         
+       }
+}
