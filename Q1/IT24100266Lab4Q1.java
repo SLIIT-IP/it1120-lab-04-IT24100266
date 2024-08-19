e072a3d9c2f12045f9d6f2ec93873cb0832565be
IT24100266 <it24100266@my.sliit.lk>
2024-08-19 16:33:53 +0530

    Add files via upload

---
+++ Q1/IT24100266Lab4Q1.java
@@ -0,0 +1,16 @@
+public class IT24100266Lab4Q1{
+     public static void main(String[]args){
+ 
+        double number = (7);
+           System.out.println("Enter a number:7");
+
+          if (number>0){
+             System.out.println("The number is: positive.");}
+          else if (number<0){
+              System.out.println("The number is: negative");}
+          else {
+             System.out.println("The number is: zero"); }
+         
+           
+        }
+}
